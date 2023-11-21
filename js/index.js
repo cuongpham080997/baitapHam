b8952caf132fd57a0d7f75491dc2a99265853750
bachlo97 <slh97dng@gmail.com>
2023-11-21 16:27:27 +0700

    complete

--- js/index.js
+++ js/index.js
@@ -24,6 +24,7 @@ function areValidNumbers(...nums) {
   });
 }
 
+
 function checkRadioButtons(...strs) {
   for (const str of strs) {
     var selectedRadioButton = document.querySelector(`input[name=${str}]:checked`);
@@ -33,6 +34,16 @@ function checkRadioButtons(...strs) {
   }
 
   return true; // Nếu tất cả các nhóm radio button đều có ít nhất một radio button được chọn, trả về true
+}
+
+function uncheckRadio(str) {
+  // Lựa chọn radio button cần bỏ chọn bằng cách sử dụng CSS selector
+  var radio = document.querySelector(`input[name=${str}]:checked`);
+
+  // Nếu có radio được chọn, bỏ chọn nó
+  if (radio) {
+    radio.checked = false;
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////?
@@ -156,22 +167,117 @@ domId('btn-2').onclick = function () {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////?
 
 //* Start lesson 3
+const THUE_SUAT_DEN_60 = 0.05;
+const THUE_SUAT_TREN_60_DEN_120 = 0.10;
+const THUE_SUAT_TREN_120_DEN_210 = 0.15;
+const THUE_SUAT_TREN_210_DEN_384 = 0.20;
+const THUE_SUAT_TREN_384_DEN_624 = 0.25;
+const THUE_SUAT_TREN_624_DEN_960 = 0.30;
+const THUE_SUAT_TREN_960 = 0.35;
 
+const CHIU_THUE_60 = 60e+6;
+const CHIU_THUE_120 = 120e+6;
+const CHIU_THUE_210 = 210e+6;
+const CHIU_THUE_384 = 384e+6;
+const CHIU_THUE_624 = 624e+6;
+const CHIU_THUE_960 = 960e+6;
+domId('btn-3').onclick = function () {
+  var hoTen = domId('hoTen').value;
+  var tongThuNhap = domId('tongThuNhap').value;
+  var soNguoiPhuThuoc = domId('nguoiPhuThuoc').value;
+  if (hoTen == '' || !isNaN(+hoTen) || !areValidNumbers(tongThuNhap, soNguoiPhuThuoc) || !Number.isInteger(+soNguoiPhuThuoc) || tongThuNhap <= (4e+6)) {
+    domId("result3").style.display = "none";
+    return;
+  }
 
+  tongThuNhap = +tongThuNhap;
+  soNguoiPhuThuoc = +soNguoiPhuThuoc;
 
+  var thuNhapChiuThue = tongThuNhap - (4e+6) - soNguoiPhuThuoc * (1.6e+6)
 
+  if (thuNhapChiuThue < 0) {
+    domId("result3").style.display = "none";
+    return
+  }
 
+  var thueThuNhapCaNhan = 0;
+  if (thuNhapChiuThue <= CHIU_THUE_60) {
+    thueThuNhapCaNhan = thuNhapChiuThue * THUE_SUAT_DEN_60;
+  } else if (thuNhapChiuThue > CHIU_THUE_60 && thuNhapChiuThue <= CHIU_THUE_120) {
+    thueThuNhapCaNhan = thuNhapChiuThue * THUE_SUAT_TREN_60_DEN_120
+  } else if (thuNhapChiuThue > CHIU_THUE_120 && thuNhapChiuThue <= CHIU_THUE_210) {
+    thueThuNhapCaNhan = thuNhapChiuThue * THUE_SUAT_TREN_120_DEN_210
+  } else if (thuNhapChiuThue > CHIU_THUE_210 && thuNhapChiuThue <= CHIU_THUE_384) {
+    thueThuNhapCaNhan = thuNhapChiuThue * THUE_SUAT_TREN_210_DEN_384
+  } else if (thuNhapChiuThue > CHIU_THUE_384 && thuNhapChiuThue <= CHIU_THUE_624) {
+    thueThuNhapCaNhan = thuNhapChiuThue * THUE_SUAT_TREN_384_DEN_624;
+  } else if (thuNhapChiuThue > CHIU_THUE_624 && thuNhapChiuThue <= CHIU_THUE_960) {
+    thueThuNhapCaNhan = thuNhapChiuThue * THUE_SUAT_TREN_624_DEN_960;
+  } else if (thuNhapChiuThue > CHIU_THUE_960) {
+    thueThuNhapCaNhan = thuNhapChiuThue * THUE_SUAT_TREN_960;
+  }
 
-
-
-
-
-
-
-
-
-
-
-
+  domId('result3').style.display = 'block'
+  domId('result3').innerHTML = `Khách hàng ${hoTen} có tiền thuế thu nhập cá nhân là ${new Intl.NumberFormat('vn-VN').format(thueThuNhapCaNhan)} VNĐ `
+}
 
 //* End lesson 3
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////?
+
+//* Start lesson 4
+function toggleInput() {
+
+  domId('connect-num').style.display = domId('business').checked ? 'flex' : 'none';
+}
+
+// Xóa chọn trạng thải của radiobutton khi load lại trang
+var domItem4 = domId('item4')
+if (!domItem4.classList.contains('open')) {
+  uncheckRadio('selector1')
+}
+
+const XU_LY_HOA_DON_HOUSE = 4.5
+const DV_CO_BAN_HOUSE = 20.5
+const KENH_CAO_CAP_HOUSE = 7.5
+
+const XU_LY_HOA_DON_BUSINESS = 15
+const DV_CO_BAN_BUSINESS = 75
+const KET_NOI_THEM = 5
+const KENH_CAO_CAP_BUSINESS = 50
+
+function checkSoKetNoi(soKetNoi, domRadio) {
+  if (domRadio.checked) {
+    if (!areValidNumbers(soKetNoi) || !Number.isInteger(+soKetNoi)) {
+      return false
+    }
+  }
+  return true
+
+}
+
+domId('btn-4').onclick = function () {
+  var maKhachhang = domId('maKhachHang').value
+  var soKenhCaoCap = domId('soKenh').value
+  var soKetNoi = domId('soKetNoi').value
+  var tienCap =0;
+
+  if (maKhachhang == '' || !areValidNumbers(soKenhCaoCap) || !Number.isInteger(+soKenhCaoCap) || !checkSoKetNoi(soKetNoi,domId('business'))) {
+    domId("result4").style.display = "none";
+    return;
+  }
+
+  soKenhCaoCap = +soKenhCaoCap
+  soKetNoi = +soKetNoi
+  if(domId('nhaDan').checked){
+    tienCap = XU_LY_HOA_DON_HOUSE + DV_CO_BAN_HOUSE + KENH_CAO_CAP_HOUSE*soKenhCaoCap
+  }else{
+    var phiDV = soKetNoi<=10 ? DV_CO_BAN_BUSINESS : DV_CO_BAN_BUSINESS+(soKetNoi-10)*KET_NOI_THEM 
+    tienCap = XU_LY_HOA_DON_BUSINESS + phiDV + KENH_CAO_CAP_BUSINESS*soKenhCaoCap
+  }
+
+  domId('result4').style.display = 'block'
+  domId('result4').innerHTML = `Mã khách hàng: ${maKhachhang} ; Tiền cáp: ${tienCap}$`
+}
+
+//* End lesson 4  
